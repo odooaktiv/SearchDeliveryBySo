4f34344fd0b87ab8a2c02f9dac3f7024215ee1c8
Odoo Aktiv <solankiriddhir@yahoo.in>
2018-06-30 11:29:59 +0530

    [IMP] pep8 standards for branch 10.0

--- search_delivery_number_so/wizard/delivery_reference_wizard.py
+++ search_delivery_number_so/wizard/delivery_reference_wizard.py
@@ -1,15 +1,18 @@
 # -*- coding: utf-8 -*-
 
-from odoo import api, fields, models, _
+from odoo import api, models
+
 
 class DeliveryReferenceSale(models.TransientModel):
     _name = 'delivery.reference.sale'
-    
+
     @api.multi
     def add_delivery_refrence_in_so(self):
         sale_id = self.env['sale.order'].search([])
         for sale in sale_id:
-            picking_list = [picking_id.name for picking_id in sale.picking_ids if picking_id]
+            picking_list = [
+                picking_id.name for picking_id in
+                sale.picking_ids if picking_id]
             for picking in picking_list:
                 if sale.delivery_number:
                     delivery_number = sale.delivery_number
